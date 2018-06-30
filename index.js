1dd6f178725309a9115f8ad921716c624d09f84b
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-06-30 18:55:43 +0100

    defs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-'use strict'
 const path = require('path')
 const kebabCase = require('lodash.kebabcase')
 
 module.exports = PluginBase => class RenamerCase extends PluginBase {
+  optionDefinitions () {
+    return [
+      { name: 'case', description: 'Possible values: camel-case, title-case, kebab-case.'}
+    ]
+  }
   replace (file) {
     const basename = path.basename(file)
     const dirname = path.dirname(file)
